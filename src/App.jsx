c8cea2747935e12eda2aec0b371faecb532ef1e8
Stuart <stuiek1985@gmail.com>
2025-04-23 14:31:49 +0100

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MicrocopyGenerator from "./components/MicrocopyGenerator";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MicrocopyGenerator />
+    </div>
+  );
+}
+
+export default App;
